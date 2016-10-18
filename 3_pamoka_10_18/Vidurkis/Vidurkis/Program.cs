8ad2c30b88e256823bed8207a945ffdb8f8dc4ea
jonasvilniuje <jonasvilniuje@gmail.com>
2016-10-18 17:51:12 +0300

    3 pamoka

---
+++ 3_pamoka_10_18/Vidurkis/Vidurkis/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidurkis
+{
+    class Program
+    {
+
+        private static decimal CalculateAverage(List<decimal> numbers)
+        {
+            decimal sum = 0;
+            decimal average = 0;
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            average = sum / numbers.Count;
+
+            return average;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Kiekis:");
+            //string value = Console.ReadLine();
+            //int n = int.Parse(value);
+           
+           // string[] number = new string[n];
+
+            List<decimal> tekstas = new List<decimal>();
+
+            while(true)
+            {
+                string sk = Console.ReadLine();
+                if(sk == "-")
+                {
+                    break;
+                }
+                tekstas.Add(decimal.Parse(sk));
+            }
+
+
+            decimal average;
+            //decimal[] numbers = new decimal[n];
+
+            //Console.WriteLine("Skaiciai:");
+            
+                //number[i] = Console.ReadLine();
+                //numbers[i] = decimal.Parse();
+
+                average = CalculateAverage(tekstas);
+
+            Console.WriteLine("Vidurkis:");
+            Console.WriteLine(average);
+            Console.ReadLine();
+        }
+    }
+}
