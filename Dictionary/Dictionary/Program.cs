bfe63f300f34791e657aabb60c2667d1b4dfcd59
Aurimasjar <aurimaspetretis@gmail.com>
2016-11-08 17:50:42 +0200

    5 pamoka

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fileTextLines = File.ReadLines(@"D:\SAUGYKLA\Aurimas 4G\Pamokos\Dictionary\Dictionary\chatBot.txt").ToList();
+            string[] splitLine;
+            Dictionary<string, string> dictionary = //new Dictionary<string, string>();
+            fileTextLines.ToDictionary(item => item.Split('~')[0].Trim(), item => item.Split('~')[1].Trim());
+
+            /*foreach (var item in fileTextLines)
+            {
+                splitLine = item.Split('~');
+                dictionary.Add(splitLine[0].Trim(), splitLine[1].Trim());
+            }*/
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if(dictionary.ContainsKey(input))
+                {
+                    Console.WriteLine(dictionary[input]);
+                }
+                if(string.Compare(input, "Quit", true) == 0)
+                {
+                    break;
+                }
+                if(!dictionary.ContainsKey(input))
+                {
+                    Console.WriteLine("Sorry, I don't have any answer :(");
+                }
+            }
+        }
+    }
+}
